955de0c4efff862fc009d33dea49c6152211cbe1
Abhir <abhir.mirikar@gmail.com>
2022-10-17 22:22:38 +0530

    Updated the views.py

--- emifyApp/views.py
+++ emifyApp/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
-from django.http import StreamingHttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
 
 from emifyApp.camera import LaptopVideoCamera 
+import time
 
 def index(request):
     return render(request,'emifyApp/home.html')
 
 def gen(camera):
-    while True: 
+    while True:
+        end = time.time() + 5 
+        # m = []
         frame = camera.get_frame() 
-        yield(b'--frame\r\n'
+        yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        # while time.time() < end: 
+        #     m.append(camera.get_result())
+        # max = 0
+        # for i in m:
+        #     if m.count(i) > max:
+        #         max = m.count(i)
+        # for i in m:
+        #     if m.count(i)==max:
+        #         print(i)
+        #         break 
+        
         
 def video_feed(request):
-    return StreamingHttpResponse(gen(LaptopVideoCamera()),content_type = 'multipart/x-mixed-replace; boundary = frame')
+    return StreamingHttpResponse(gen(LaptopVideoCamera()),content_type = 'multipart/x-mixed-replace; boundary=frame')
+
+def getR(request): 
+    return 
